04b112f215fa7929afa91b63f1cd1bee42423519
irfan <116457057+fann22@users.noreply.github.com>
2025-07-16 19:44:12 +0700

    Update XposedInit.java

--- app/src/main/java/com/github/dan/NoStorageRestrict/XposedInit.java
+++ app/src/main/java/com/github/dan/NoStorageRestrict/XposedInit.java
@@ -1,31 +1,11 @@
 package com.github.dan.NoStorageRestrict;
 
-import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
-import static de.robv.android.xposed.XposedHelpers.findMethodBestMatch;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
-
-import static de.robv.android.xposed.XposedHelpers.callMethod;
-import static de.robv.android.xposed.XposedHelpers.findAndHookConstructor;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class XposedInit implements IXposedHookLoadPackage {
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-
-        if (Constants.DOCUMENTSUI_PACKAGE_NAME.equals(lpparam.packageName)) {
-            new ManagedModehooks().handleLoadPackage(lpparam);
-        }
-
-        if (Constants.DOCUMENTSUI_GOOGLE_PACKAGE_NAME.equals(lpparam.packageName)) {
-            new ManagedModehooks().handleLoadPackage(lpparam);
-        }
-
-        if (Constants.STORAGEMANAGER_NAME.equals(lpparam.packageName)) {
-            new FolderRestrictionhook().handleLoadPackage(lpparam);
-        }
-
-        if (Constants.STORAGEMANAGER_NAME.equals(lpparam.packageName)) {
-            new DocumentHideBypass().handleLoadPackage(lpparam);
-        }
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
     }
 }
